b976d6538ff3e92e52d916dfc889d42842fbcfc3
Liam Rayback <liam@macbook.local>
2024-11-25 07:00:16 -0700

    login and sign up pages now render and load correctly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Login from './pages/Login';
+import SignUp from './pages/signUpPage';
+import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+const Layout = () => (
+  <div className="container">
+    <Navbar /> {/* Navbar stays on all pages */}
+    <Outlet /> {/* Renders the matched child route's element */}
+  </div>
+);
 
 function App() {
-
   return (
-    <div className='container'>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  )
+    <Routes>
+      {/* Shared layout */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Login />} /> {/* Default page */}
+        <Route path="signUpPage" element={<SignUp />} />
+        <Route path="ErrorPage" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
+
