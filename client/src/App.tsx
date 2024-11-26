71866392a7a6b609c4f5fac2b4a5fc6d44462b12
Liam Rayback <liam@macbook.local>
2024-11-25 20:20:22 -0700

    updates to app.tsx to include latest pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import ErrorPage from './pages/ErrorPage';
 import Login from './pages/Login';
 import SignUp from './pages/signUpPage';
 import Navbar from './components/Navbar';
+import Home from './pages/home';
+import MyCollection from './pages/myCollection';
+import AlbumDetails from './pages/albumDetails';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => (
@@ -20,6 +23,9 @@ function App() {
         <Route index element={<Login />} /> {/* Default page */}
         <Route path="signUpPage" element={<SignUp />} />
         <Route path="ErrorPage" element={<ErrorPage />} />
+        <Route path="home" element={<Home />} />
+        <Route path="myCollection" element={<MyCollection />} />
+        <Route path="albumDetails" element={<AlbumDetails />} />
       </Route>
     </Routes>
   );
