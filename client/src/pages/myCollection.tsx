7d142cc445923f0806319b050442413face78f6a
milkymilk <fluffypuffmalloween@gmail.com>
2024-11-26 19:19:46 -0700

    added kanye api to footer, updated code to allow for users to successfully sign in and create user and to go to the correct route afterwards

--- client/src/pages/myCollection.tsx
+++ client/src/pages/myCollection.tsx
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react';
-import { CollectionContext } from '../context/CollectionContext';
-import CollectionGrid from '../components/CollectionGrid';
+// import { useContext } from 'react';
+// import { CollectionContext } from '../context/CollectionContext';
+// import CollectionGrid from '../components/CollectionGrid';
 
 const MyCollection = () => {
-  const { albums } = useContext(CollectionContext);
+//   const { albums } = useContext(CollectionContext);
 
   return (
     <div className="container mx-auto p-6 bg-gray-800 text-white rounded-lg">
       <h1 className="text-3xl font-bold mb-6">My Record Collection</h1>
-      {albums.length > 0 ? (
+      {/* {albums.length > 0 ? (
         <CollectionGrid />
       ) : (
         <p className="text-lg">Your collection is empty. Start adding albums!</p>
-      )}
+      )} */}
     </div>
   );
 };
