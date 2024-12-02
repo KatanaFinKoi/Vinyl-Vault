9df8e9b239baed0171a9545f56ee6c88ba68752d
Liam Rayback <liam@macbook.local>
2024-12-02 15:54:36 -0700

    reverting changes

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -8,13 +8,10 @@ import { AlbumFactory } from './albums.js'
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
       },
     });
 
